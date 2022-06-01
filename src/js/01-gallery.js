e2a406d205aa81d8cce8225739e6117abe93a5d5
Ioanna-Istomina <99349464+Ioanna-Istomina@users.noreply.github.com>
2022-06-02 00:08:33 +0300

    add first and third exercises

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+//Ключ для сховища
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onTextInput, 500));
+form.addEventListener('submit', resetForm);
+
+//Об'єкт даних з форми
+const formData = {};
+
+populateInput();
+
+//Функція при якій записується значення форми в сховище
+function onTextInput(ev) {
+  formData[ev.target.name] = ev.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+//Функція для отримання значення форми зі сховища
+function populateInput() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedMessage) {
+    email.value = savedMessage.email;
+    message.value = savedMessage.message;
+  }
+}
+
+// Функція для очищення форми та сховища при submit
+function resetForm(ev) {
+  //виводить в консоль обьект з email та message
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  ev.preventDefault();
+  ev.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
